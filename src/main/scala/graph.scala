256401ed34f1e9d22df1bd6e3668015b7b576028
Harry <hzhu20@georgefox.edu>
2022-02-07 21:05:03 -0800

    Finish program

--- src/main/scala/graph.scala
+++ src/main/scala/graph.scala
@@ -49,33 +49,115 @@ object graph
 		An private implmenentation of the Graph trait
 		*/
 		//TODO add in data structures to the constructor here
+
+
+		
 		//you could also consider a different implementation for directed vs undirected graphs...
 		private class GraphImpl[T] (val isDirected:Boolean) extends Graph[T]
 		{
-			def getVertices:Iterable[T] = Seq()
+			//TODO add in data structures here
+			private var vertices:List[T] = List()
+			private var edges:List[(T, T, Int)] = List()
 
-			def edgeExists(source:T, destination:T):Boolean = false
+			def getVertices:Iterable[T] = vertices
+
+			def edgeExists(source:T, destination:T):Boolean = {
+				edges.exists(e => e._1 == source && e._2 == destination)
+			}
 			
-			def getEdgeWeight(source:T, destination:T):Option[Int] = None
+			def getEdgeWeight(source:T, destination:T):Option[Int] = {
+				edges.find(e => e._1 == source && e._2 == destination).map(_._3)
+			}
 
-			def addVertex(vertex:T):Graph[T] = null
+			def addVertex(vertex:T):Graph[T] = {
+				if (vertices.contains(vertex))
+					throw new IllegalArgumentException("Vertex already exists")
+				else
+					vertices = vertex :: vertices
+
+				this
+			}
 			
-			def removeVertex(vertex:T):Graph[T] = null
+			def removeVertex(vertex:T):Graph[T] = {
+				if (!vertices.contains(vertex))
+					throw new IllegalArgumentException("Vertex does not exist")
+				else
+				{
+					// remove vertex
+					vertices = vertices.filterNot(_ == vertex)
 
-			def addEdge(source:T, destination:T, weight:Int):Graph[T] = null
+					// remove edges that have this vertex as a source or destination
+					edges = edges.filterNot(e => e._1 == vertex || e._2 == vertex)
+				}
 
-			def removeEdge(source:T, destination:T):Graph[T] = null
+				this
+			}
 
-			override def toString:String = "Empty graph..."
+			def addEdge(source:T, destination:T, weight:Int):Graph[T] = {
+				if (!vertices.contains(source) || !vertices.contains(destination))
+					throw new IllegalArgumentException("Vertex does not exist")
+				else if (edgeExists(source, destination))
+					throw new IllegalArgumentException("Edge already exists")
+				else
+				{
+					edges = (source, destination, weight) :: edges
+					if (!isDirected)
+						edges = (destination, source, weight) :: edges
+				}
+
+				this
+			}
+
+			def removeEdge(source:T, destination:T):Graph[T] = {
+				if (!vertices.contains(source) || !vertices.contains(destination))
+					throw new IllegalArgumentException("Vertex does not exist")
+				else if (!edgeExists(source, destination))
+					throw new IllegalArgumentException("Edge does not exist")
+				else
+				{
+					edges = edges.filterNot(e => e._1 == source && e._2 == destination)
+					if (!isDirected)
+						edges = edges.filterNot(e => e._1 == destination && e._2 == source)
+				}
+
+				// return this graph
+				this
+			}
+
+			override def toString:String = {
+				val sb = new StringBuilder
+				sb.append("Vertices: ")
+				vertices.foreach(v => sb.append(v + ", "))
+				sb.append("\nEdges: ")
+				edges.foreach(e => sb.append(e._1 + " -> " + e._2 + ", "))
+				sb.toString
+			}
 		}
 	}
 
 	def main(args:Array[String])
 	{
 		//create an empty graph
-		val exampleGraph = Graph[String](false)
-		
-		//print it out
-		println(exampleGraph)
+		val undirectedGraph = Graph[String](false)
+
+		//add some vertices of type String
+		undirectedGraph.addVertex("A").addVertex("B").addVertex("C")
+
+		//add some edges of type String
+		undirectedGraph.addEdge("A", "B", 1).addEdge("A", "C", 2)
+
+		//print the graph
+		println(undirectedGraph)
+
+		val directedGraph = Graph[String](true)
+
+		directedGraph.addVertex("A").addVertex("B").addVertex("C")
+
+		directedGraph.addEdge("A", "B", 1).addEdge("A", "C", 2)
+
+		// remove a vertex
+		directedGraph.removeVertex("B")
+
+		println(directedGraph)
 	}
 }
